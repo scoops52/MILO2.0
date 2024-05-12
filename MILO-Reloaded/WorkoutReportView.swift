fef9d7446b6accb73a7ffc78af7b0278c6f39981
Sean Cooper <seancooperwd@gmail.com>
2024-05-11 18:16:24 -0700

    Added picker for hours and minutes with computed totalMinutes var

--- MILO-Reloaded/WorkoutReportView.swift
+++ MILO-Reloaded/WorkoutReportView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct WorkoutReportView: View {
     @State private var workoutDate = Date()
-    @State private var workoutMinutes = 1
+    @State private var selectedHours = 0
+    @State private var selectedMinutes = 45
     @State private var workoutIntensity = 5.0
     
-    let minuteOptions = Array(1...200)
+    let hourOptions = Array(0...23)//Available hour options to select
+    let minuteOptions = Array(0...59)// Available minute options to select
+    
+    
+    //total minutes of workout computed from selected hours and minutes
+    var totalMinutes: Int {
+        return selectedHours * 60 + selectedMinutes
+    }
     
     
     var body: some View {
@@ -21,11 +29,21 @@ struct WorkoutReportView: View {
                 Section(header: Text("Report Workout")) {
                     DatePicker("Workout Date", selection: $workoutDate, displayedComponents: .date)
                     
-                    Picker("Workout Duration (minutes)", selection: $workoutMinutes) {
-                        ForEach(minuteOptions, id: \.self) { minute in
-                            Text("\(minute) minutes")
-                        }
-                    }
+                    HStack {
+                                            Picker("Hours", selection: $selectedHours) {
+                                                ForEach(hourOptions, id: \.self) { hour in
+                                                    Text("\(hour) hours")
+                                                }
+                                            }
+                                            .pickerStyle(WheelPickerStyle())
+                                            
+                                            Picker("Minutes", selection: $selectedMinutes) {
+                                                ForEach(minuteOptions, id: \.self) { minute in
+                                                    Text("\(minute) minutes")
+                                                }
+                                            }
+                                            .pickerStyle(WheelPickerStyle())
+                                        }
                     
                     Stepper(value: $workoutIntensity, in: 1.0...10.0, step: 0.5) {
                         Text("Workout Intensty: \(workoutIntensity, specifier: "%.1f")")
@@ -45,7 +63,7 @@ struct WorkoutReportView: View {
     }
     func submitWorkout(){
         print("Workout Date: \(workoutDate)")
-        print("Workout Duration: \(workoutMinutes) minutes")
+        print("Workout Duration: \(totalMinutes) minutes")
         print("Workout Intensity: \(workoutIntensity)")
     }
 }
