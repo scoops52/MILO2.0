c3c73eb54e8aa24e612e62e0c897525036975d58
Sean Cooper <seancooperwd@gmail.com>
2024-05-11 17:37:55 -0700

    Initial Commit

---
+++ MILO-Reloaded/MILO_ReloadedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MILO_ReloadedApp.swift
+//  MILO-Reloaded
+//
+//  Created by Sean Cooper on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MILO_ReloadedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
